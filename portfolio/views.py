e5283d2efb7d285c122a21c0d9d5cd15a2407323
Brian Koko <kodenova.tech@gmail.com>
2025-12-10 03:26:42 +0000

    update fea/views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Project, GalleryItem
 from .forms import ContactForm
-from bolt import run_flow
+
 
 def index(request):
     return render(request, "portfolio/index.html", {
