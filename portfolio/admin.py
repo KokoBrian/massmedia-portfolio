f37c0ed231d0c6349c392590e86ef833827252f7
Brian Koko <kodenova.tech@gmail.com>
2025-12-10 03:21:26 +0000

    updates

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project, GalleryItem
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'media_type', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('media_type',)
+
+@admin.register(GalleryItem)
+class GalleryItemAdmin(admin.ModelAdmin):
+    list_display = ('caption', 'created_at')
